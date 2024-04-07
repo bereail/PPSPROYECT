c768bcefd81dddec81bbd84c7cc7cced995d0761
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-04-07 05:18:00 -0300

    correccion

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,14 +1,68 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Navbar as  Nav } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from 'react-router-dom';
+
 
 const Login = () => {
-  return (<></>
-    // <div style={{ border: "1px solid #ccc", borderRadius: "10px", padding: "4.5px", width: "200px" }}>
-    //   <div style={{ display: "flex", gap: "3px" }}>
-    //     <Link to="/signupUser" type="button" className="btn btn-outline-primary">Registrarse</Link>
-    //     <Link to="/signin" type="button" className="btn btn-outline-primary">Iniciar Sesión</Link>
-    //   </div>
-    // </div>
+  const [isExpanded, setIsExpanded] = useState(false);
+  const { pathname } = useLocation();
+  const [currentPage, SetCurrentPage] = useState('Home');
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        SetCurrentPage('Home');
+        break;
+      case '/signupUser':
+        SetCurrentPage('Register');
+        break;
+      case '/signin':
+        SetCurrentPage('Login');
+        break;
+      case '/cart':
+        SetCurrentPage('cart');
+        break;
+      default:
+        SetCurrentPage('');
+    }
+  }, [pathname]);
+
+
+  return (
+    <div>
+
+      {currentPage !== 'Login' && currentPage !== 'Register' && (
+        <div style={{ marginRight: currentPage === 'cart' && "100px" }} className="filter-bar-container">
+          <button className='icon-category' onMouseEnter={() => setIsExpanded(true)} onMouseLeave={() => setIsExpanded(false)}>  My Count</button>
+          {isExpanded && (
+            <div className="filter-bar expanded" onMouseEnter={() => setIsExpanded(true)} onMouseLeave={() => setIsExpanded(false)} style={{ backgroundColor: 'white', border: '1px solid #ccc', boxShadow: '0 2px 4px rgba(0,0,0,0.1)', padding: '10px', }}>
+              <Nav className="ml-auto">
+                <div style={{ marginRight: '5px', width: '100px' }} onClick={() => SetCurrentPage('Register')}>
+                  {currentPage !== 'Register' && (
+                    <Link to="/signupUser" type="button" className="btn btn-outline-primary"> Sing Up</Link>
+                  )}
+                </div>
+                <div style={{ width: '80px' }} onClick={() => SetCurrentPage('Login')} >
+                  {currentPage !== 'Login' && (
+                    <Link to="/signin" type="button" className="btn btn-outline-primary" >Sing In</Link>
+                  )}
+                </div>
+              </Nav>
+            </div>
+          )}
+        </div>
+      )}
+      {currentPage == 'Login' && (
+        <Link to="/signupUser" type="button" className="btn btn-outline-primary"> Sing Up</Link>
+      )}
+      {currentPage == 'Register' && (
+        <Link to="/signin" type="button" className="btn btn-outline-primary" >Sing In</Link>
+      )}
+
+
+    </div>
+
   );
 };
 
--- src/components/Navbar/CustomNavbar.js
+++ src/components/Navbar/CustomNavbar.js
@@ -11,7 +11,7 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import FilterBar from "../FilterBar/FilterBar";
 
 const CustomNavbar = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
+
   const { pathname } = useLocation();
   const [currentPage, SetCurrentPage] = useState('Home');
 
@@ -33,9 +33,6 @@ const CustomNavbar = () => {
         SetCurrentPage('');
     }
   }, [pathname]);
-  const handlePageChange = (pageName) => {
-    SetCurrentPage(pageName)
-  }
 
   return (
     <BootstrapNavbar className="navbar" style={{ backgroundImage: "linear-gradient(to right,#AFDFF3, #8AB5E8)", borderBottomLeftRadius: "15%", borderBottomRightRadius: "15%" }}>
@@ -43,49 +40,21 @@ const CustomNavbar = () => {
       <Container fluid style={{ maxHeight: '50px', padding: '0px' }}>
 
 
-        <Link to='/' onclick={() => handlePageChange('Home')}>  <Logo /> </Link>
-        <Link to='/' onclick={() => handlePageChange('Home')} type="button" className="btn btn-outline-primary">  Home </Link>
+        <Link to='/' onclick={() => SetCurrentPage('Home')}>  <Logo /> </Link>
+        <Link to='/' onclick={() => SetCurrentPage('Home')} type="button" className="btn btn-outline-primary">  Home </Link>
         <Nav className="flex-grow-1 d-flex justify-content-center"> {/* Utiliza flex-grow-1 para que el Search ocupe todo el espacio disponible */}
 
-          <div style={{ display: 'inline-flex' }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              {currentPage === 'Home' && (
-                <FilterBar ></FilterBar>
 
-              )}
-            </div>
-            <div style={{ display: 'inline-flex', alignItems: 'center' }}>{currentPage === 'Home' && <Search />}</div>
-          </div>
-        </Nav>
-        {currentPage !== 'Login' && currentPage !== 'Register' && (
-          <div style={{ marginRight: currentPage === 'cart' && "100px"}} className="filter-bar-container">
-            <button className='icon-category' onMouseEnter={() => setIsExpanded(true)} onMouseLeave={() => setIsExpanded(false)}>  My Count</button>
-            {isExpanded && (
-              <div className="filter-bar expanded" onMouseEnter={() => setIsExpanded(true)} onMouseLeave={() => setIsExpanded(false)} style={{ backgroundColor: 'white', border: '1px solid #ccc', boxShadow: '0 2px 4px rgba(0,0,0,0.1)', padding: '10px', }}>
-                <Nav className="ml-auto">
-                  <div style={{ marginRight: '5px', width: '100px' }} onClick={() => handlePageChange('Register')}>
-                    {currentPage !== 'Register' && (
-                      <Link to="/signupUser" type="button" className="btn btn-outline-primary"> Sing Up</Link>
-                    )}
-                  </div>
-                  <div style={{ width: '80px' }} onClick={() => handlePageChange('Login')} >
-                    {currentPage !== 'Login' && (
-                      <Link to="/signin" type="button" className="btn btn-outline-primary" >Sing In</Link>
-                    )}
-                  </div>
-                </Nav>
-              </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {currentPage === 'Home' && (
+              <FilterBar ></FilterBar>
             )}
           </div>
-        )}
+          <div>{currentPage === 'Home' && <Search />}</div>
 
-        {currentPage == 'Login' && (
-          <Link to="/signupUser" type="button" className="btn btn-outline-primary"> Sing Up</Link>
-        )}
-          {currentPage == 'Register' && (
-                      <Link to="/signin" type="button" className="btn btn-outline-primary" >Sing In</Link>
-        )}
+        </Nav>
 
+        <Login></Login>
         {currentPage === 'Home' && (
 
           <Link to="/cart" style={{ color: 'black', fontSize: '25px', textDecoration: 'none', backgroundColor: '#60a2e7', padding: '14px', borderBottomRightRadius: '15%', marginLeft: "30px" }}>
@@ -93,6 +62,7 @@ const CustomNavbar = () => {
           </Link>
 
         )}
+
       </Container>
     </BootstrapNavbar>
   );
