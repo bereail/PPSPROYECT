4a9bcd0e827724e65abb41a950be6335d18e3173
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-05-01 16:43:49 -0300

    intento de conexion de back y front

--- minimarket-client/src/components/Home/home.js
+++ minimarket-client/src/components/Home/home.js
@@ -4,6 +4,7 @@ import CarrouselPage from "../Carrousel/CarrouselPage/CarrouselPage";
 import Footer from "../Footer/footer";
 import Header from "../Header/header";
 import "./Home.css";
+import GetProducts from "../Products/GetProducts";
 const Home = () => {
   return (
     <div className="Home">
@@ -11,10 +12,11 @@ const Home = () => {
       <CustomNavbar />
 
       <CarrouselPage />
-
+      
       <div className="content">
 
         <Header />
+        <GetProducts></GetProducts>
       </div>
       <Footer />
     </div>
