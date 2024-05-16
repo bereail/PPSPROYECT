43a7f59351c1ac48b9a1af5ec15338d4dccb4a3e
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-05-16 08:37:47 -0300

    prueba

---
+++ minimarket-client/src/Api.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const getToken = () => {
+    return window.localStorage.getItem('LoggedUser'); 
+};
+
+const Api = () => {
+    const token = getToken('');
+
+    const api = axios.create({
+        baseURL: 'https://localhost:7191',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    return api;
+};
+
+export default Api;
